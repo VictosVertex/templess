fc2d2490951a130c25c63eac46508f0600c889da
VictosVertex <Victos.Vertex@gmail.com>
2026-02-04 17:57:51 +0100

    add error screen component

---
+++ src/gui/components/error.rs
@@ -0,0 +1,14 @@
+//! This module defines an error component to display error messages in the GUI.
+
+use dioxus::prelude::*;
+
+/// A simple error component that displays an error message.
+#[component]
+pub fn Error(title: String, error:String) -> Element {
+    rsx! {
+        div { class: "flex flex-col items-center justify-center gap-4 bg-background",
+            h1 { class: "text-2xl font-bold text-error", "{title}" }
+            p { class: "text-error", "{error}" }
+        }
+    }
+}
