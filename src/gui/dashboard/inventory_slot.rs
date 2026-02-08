2416200d170dc7bd1f18c9f856a495c23bdf2403
VictosVertex <Victos.Vertex@gmail.com>
2026-02-08 23:20:43 +0100

    add button on hover to remove item from slot

--- src/gui/dashboard/inventory_slot.rs
+++ src/gui/dashboard/inventory_slot.rs
@@ -5,12 +5,14 @@ use crate::core::domain::item_slot::ItemSlot;
 use crate::gui::components::modal::ModalContext;
 use crate::gui::dashboard::item_selection_modal::ItemSelectionModal;
 use dioxus::prelude::*;
+use dioxus_free_icons::{Icon, icons::ld_icons::LdX};
 
 /// Properties for the InventorySlot component.
 #[derive(Props, Clone, PartialEq)]
 pub struct InventorySlotProps {
     /// Optional CSS class for additional styling.
-    class: Option<String>,
+    #[props(default)]
+    class: String,
 
     /// The type of item slot this inventory slot represents.
     slot_type: ItemSlot,
@@ -25,7 +27,7 @@ pub struct InventorySlotProps {
 #[component]
 pub fn InventorySlot(props: InventorySlotProps) -> Element {
     let mut modal_ctx = use_context::<Signal<ModalContext>>();
-    let app_state = use_context::<Signal<AppState>>();
+    let mut app_state = use_context::<Signal<AppState>>();
 
     let load_items = move |_| {
         let content = rsx! {
@@ -45,25 +47,51 @@ pub fn InventorySlot(props: InventorySlotProps) -> Element {
     let has_item = maybe_item.read().is_some();
     let slot_name = props.slot_type.name().replace('_', " ");
 
-    let props_class = match props.class.clone() {
-        Some(class) => class,
-        None => String::new(),
+    let state_classes = if has_item {
+        "bg-accent/50 hover:bg-accent/20 hover:border-accent"
+    } else {
+        "bg-card hover:bg-accent/20 hover:border-accent"
     };
+
+    let container_classes = format!(
+        "rounded-b-full flex items-center justify-center border border-border transition-all duration-200 relative {} {}",
+        state_classes,
+        props.class
+    );
+
+    let remove_item = move |event: Event<MouseData>| {
+        event.stop_propagation();
+
+        let state = app_state.write();
+
+        if let Ok(mut guard) = state.template.lock()
+            && let Some(template) = guard.as_mut()
+        {
+            template.remove_item(&props.slot_type);
+        }
+    };
+
     rsx! {
-        button {
-            class: "flex flex-col items-center justify-center cursor-pointer capitalize",
+        div {
+            class: "flex flex-col items-center justify-center cursor-pointer group relative",
             onclick: load_items,
-            "{slot_name}"
-            div {
-                class: format!(
-                    " rounded-b-full flex items-center justify-center border border-border transition-all duration-200 {props_class} {}",
-                    if has_item {
-                        "bg-accent/50 hover:bg-accent/20 hover:border-accent"
-                    } else {
-                        "bg-card hover:bg-accent/20 hover:border-accent"
-                    },
-                ),
+
+            span { class: "capitalize mb-1", "{slot_name}" }
+            div { class: container_classes,
                 {props.icon}
+
+                if has_item {
+                    div { class: "absolute -top-1 -right-1 z-10 hidden group-hover:flex",
+
+                        button {
+                            class: "rounded-full p-0.5 w-5 h-5 flex items-center cursor-pointer justify-center hover:text-error",
+                            onclick: remove_item,
+                            title: "Unequip Item",
+
+                            Icon { icon: LdX }
+                        }
+                    }
+                }
             }
         }
     }
